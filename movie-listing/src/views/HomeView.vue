56008fbdf5401e6e68088467e4a1ce4cd2bf1067
rajanazirul <rajanazirul93@gmail.com>
2022-04-29 02:20:22 +0800

    add vuetify beta

--- movie-listing/src/views/HomeView.vue
+++ movie-listing/src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <hello-world />
 </template>
+
+<script setup lang="ts">
+import HelloWorld from "../components/HelloWorld.vue";
+
+</script>
