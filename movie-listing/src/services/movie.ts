7a8ce374eeced46360c2a91d0029f2b16fd2649e
rajanazirul <rajanazirul93@gmail.com>
2022-04-29 23:33:41 +0800

    setup composition API

--- movie-listing/src/services/movie.ts
+++ movie-listing/src/services/movie.ts
@@ -1,15 +1,54 @@
-import { ref } from "vue";
+import { computed, ref, unref, watch } from "vue";
 
-export function useMovieApi(title: string, page: number) {
-  const data = ref(null);
+export interface Filter {
+  title: string;
+  pages: number;
+}
+
+export interface Movies {
+  Title: string;
+  Year: number;
+  imdbID: string;
+}
+
+export interface Info {
+  page: number;
+  per_page: number;
+  total_pages: number;
+  data: Array<Movies>;
+}
+
+export function useMovieApi(filter: Filter) {
+  const data = ref<Info>();
   const error = ref(null);
 
-  fetch(
-    `https://jsonmock.hackerrank.com/api/movies/search/?Title=${title}&page=${page}`
-  )
-    .then((res) => res.json())
-    .then((json) => (data.value = json))
-    .catch((err) => (error.value = err));
+  const movies = computed(() => {
+    const v = unref(data);
+    return v?.data;
+  });
+  const total_pages = computed(() => {
+    const v = unref(data);
+    return v?.total_pages;
+  });
 
-  return { data, error };
+  async function getData() {
+    await fetch(
+      `https://jsonmock.hackerrank.com/api/movies/search/?Title=${filter.title}`
+    )
+      .then((res) => res.json())
+      .then((json) => (data.value = json))
+      .catch((err) => (error.value = err));
+  }
+
+  async function getDataPage() {
+    await fetch(
+      `https://jsonmock.hackerrank.com/api/movies/search/?Title=${filter.title}&page=${filter.pages}`
+    )
+      .then((res) => res.json())
+      .then((json) => (data.value = json))
+      .catch((err) => (error.value = err));
+  }
+
+  watch(filter, () => getData());
+  return { movies, total_pages, error, getData, getDataPage };
 }
