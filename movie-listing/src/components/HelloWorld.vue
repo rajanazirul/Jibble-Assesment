9841729cf15e2abcd738748445ee22bcaa099d0d
rajanazirul <rajanazirul93@gmail.com>
2022-04-29 23:34:34 +0800

    Display movie data on Home

--- movie-listing/src/components/HelloWorld.vue
+++ movie-listing/src/components/HelloWorld.vue
@@ -1,136 +1,55 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img :src="logo" class="my-3" contain height="200" />
-      </v-col>
+    <v-text-field
+      v-model="filter.title"
+      class="mr-2 py-2"
+      hide-details="auto"
+      label="Search for Movie"
+      append-icon="mdi-magnify"
+    />
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify 3 Beta
-        </h1>
+    <v-table theme="dark">
+      <thead>
+        <tr>
+          <th class="text-left">IMDB</th>
+          <th class="text-left">Title</th>
+          <th class="text-left">Year</th>
+          <th class="text-left">Favourite</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in movies" :key="item.imdbID">
+          <td>{{ item.imdbID }}</td>
+          <td>{{ item.Title }}</td>
+          <td>{{ item.Year }}</td>
+          <v-switch v-model="sw" :label="`Campaign Status`"></v-switch>
+        </tr>
+      </tbody>
+    </v-table>
 
-        <h4>Vite Preview</h4>
+    <v-pagination
+      v-model="filter.pages"
+      :length="total_pages"
+      @click="getDataPage"
+    ></v-pagination>
 
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-5">What's next?</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-5">Important Links</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-5">Ecosystem</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
+    <!-- {{ movies }} -->
   </v-container>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { reactive, ref } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
+import { useMovieApi, Filter } from "../services/movie";
 
-// Logo
-import logo from "../assets/logo.svg";
-
-export default defineComponent({
-  name: "HelloWorld",
-
-  data() {
-    return {
-      ecosystem: [
-        {
-          text: "vuetify-loader",
-          href: "https://github.com/vuetifyjs/vuetify-loader",
-        },
-        {
-          text: "github",
-          href: "https://github.com/vuetifyjs/vuetify",
-        },
-        {
-          text: "awesome-vuetify",
-          href: "https://github.com/vuetifyjs/awesome-vuetify",
-        },
-      ],
-      importantLinks: [
-        {
-          text: "Chat",
-          href: "https://community.vuetifyjs.com",
-        },
-        {
-          text: "Made with Vuetify",
-          href: "https://madewithvuejs.com/vuetify",
-        },
-        {
-          text: "Twitter",
-          href: "https://twitter.com/vuetifyjs",
-        },
-        {
-          text: "Articles",
-          href: "https://medium.com/vuetify",
-        },
-      ],
-      logo,
-      whatsNext: [
-        {
-          text: "Explore components",
-          href: "https://vuetifyjs.com",
-        },
-        {
-          text: "Roadmap",
-          href: "https://vuetifyjs.com/en/introduction/roadmap/",
-        },
-        {
-          text: "Frequently Asked Questions",
-          href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-        },
-      ],
-    };
-  },
+const filter = reactive<Filter>({
+  title: "",
+  pages: 0,
 });
+
+const sw = ref();
+
+const { movies, total_pages, getData, getDataPage } = useMovieApi(filter);
+
+onMounted(getData);
 </script>
