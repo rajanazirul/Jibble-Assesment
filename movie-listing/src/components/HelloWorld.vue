916a99b41ebfc22c3265be4cbeb170910928f3df
rajanazirul <rajanazirul93@gmail.com>
2022-04-30 06:31:28 +0800

    add Favourite features

--- movie-listing/src/components/HelloWorld.vue
+++ movie-listing/src/components/HelloWorld.vue
@@ -22,7 +22,30 @@
           <td>{{ item.imdbID }}</td>
           <td>{{ item.Title }}</td>
           <td>{{ item.Year }}</td>
-          <v-switch v-model="sw"></v-switch>
+          <v-btn
+            v-if="item.status"
+            icon
+            @click="
+              () => {
+                removeFavourite(item.imdbID);
+                getDataPage();
+              }
+            "
+          >
+            <v-icon color="green">mdi-star</v-icon>
+          </v-btn>
+          <v-btn
+            v-else
+            icon
+            @click="
+              () => {
+                setFavourite(item.imdbID);
+                getDataPage();
+              }
+            "
+          >
+            <v-icon color="white">mdi-star</v-icon>
+          </v-btn>
         </tr>
       </tbody>
     </v-table>
@@ -33,23 +56,64 @@
       @click="getDataPage"
     ></v-pagination>
 
-    <!-- {{ movies }} -->
+    <v-table theme="dark">
+      <thead>
+        <tr>
+          <th class="text-left">IMDB</th>
+          <th class="text-left">Title</th>
+          <th class="text-left">Year</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="items in favouriteName" :key="items.imdbID">
+          <td>{{ items.imdbID }}</td>
+          <td>{{ items.Title }}</td>
+          <td>{{ items.Year }}</td>
+          <v-btn
+            icon
+            @click="
+              () => {
+                removeFavourite(items.imdbID);
+                getDataPage();
+              }
+            "
+          >
+            <v-icon color="red">mdi-delete</v-icon>
+          </v-btn>
+        </tr>
+      </tbody>
+    </v-table>
   </v-container>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from "@vue/reactivity";
-import { onMounted } from "@vue/runtime-core";
-import { useMovieApi, Filter } from "../services/movie";
+import { reactive, ref, unref } from "@vue/reactivity";
+import { computed, onMounted, watch } from "vue";
+import { useMovieApi, Filter, useFavourite, Movies } from "../services/movie";
+
+const favouriteLists = ref<Array<Movies>>();
 
 const filter = reactive<Filter>({
   title: "",
   pages: 0,
 });
 
-const sw = ref();
+const favouriteName = computed(() => {
+  const v = unref<Array<Movies>>(favouriteLists);
+  return v;
+});
 
 const { movies, total_pages, getData, getDataPage } = useMovieApi(filter);
+
+const { setFavourite, removeFavourite, getDataFavourite } = useFavourite();
+
+watch(
+  movies,
+  () => getDataFavourite().then((res) => (favouriteLists.value = res)),
+  {
+    immediate: true,
+  }
+);
 
 onMounted(getData);
 </script>
--- movie-listing/src/services/movie.ts
+++ movie-listing/src/services/movie.ts
@@ -9,6 +9,7 @@ export interface Movies {
   Title: string;
   Year: number;
   imdbID: string;
+  status: boolean;
 }
 
 export interface Info {
@@ -19,15 +20,25 @@ export interface Info {
 }
 
 export function useMovieApi(filter: Filter) {
-  const data = ref<Info>();
+  const datas = ref<Info>();
   const error = ref(null);
 
   const movies = computed(() => {
-    const v = unref(data);
-    return v?.data;
+    const { favouriteData } = useFavourite();
+    const v = unref(datas);
+    const movieData = v?.data || [];
+    movieData.map((element) => {
+      favouriteData.filter((i: string) => {
+        if (i === element.imdbID) {
+          element.status = true;
+        }
+      });
+    });
+    return movieData;
   });
+
   const total_pages = computed(() => {
-    const v = unref(data);
+    const v = unref(datas);
     return v?.total_pages;
   });
 
@@ -36,7 +47,7 @@ export function useMovieApi(filter: Filter) {
       `https://jsonmock.hackerrank.com/api/movies/search/?Title=${filter.title}`
     )
       .then((res) => res.json())
-      .then((json) => (data.value = json))
+      .then((json) => (datas.value = json))
       .catch((err) => (error.value = err));
   }
 
@@ -45,7 +56,7 @@ export function useMovieApi(filter: Filter) {
       `https://jsonmock.hackerrank.com/api/movies/search/?Title=${filter.title}&page=${filter.pages}`
     )
       .then((res) => res.json())
-      .then((json) => (data.value = json))
+      .then((json) => (datas.value = json))
       .catch((err) => (error.value = err));
   }
 
@@ -55,12 +66,45 @@ export function useMovieApi(filter: Filter) {
 
 export function useFavourite() {
   const favourite = localStorage.getItem("favourite");
-  const data = favourite ? JSON.parse(favourite) : [];
+  const favouriteData = favourite ? JSON.parse(favourite) : [];
+  const error = ref(null);
 
   function setFavourite(imdbID: string) {
-    data.push(imdbID);
-    localStorage.setItem("favourite", JSON.stringify(data));
+    const index = favouriteData.indexOf(imdbID);
+    if (index == -1) {
+      favouriteData.push(imdbID);
+    }
+    localStorage.setItem("favourite", JSON.stringify(favouriteData));
   }
 
-  return { favourite, setFavourite };
+  function removeFavourite(imdbID: string) {
+    const index = favouriteData.indexOf(imdbID);
+    if (index > -1) {
+      favouriteData.splice(index, 1);
+    }
+    localStorage.setItem("favourite", JSON.stringify(favouriteData));
+  }
+
+  async function getDataFavourite() {
+    const favouriteList = ref<Array<object>>([]);
+    favouriteData.forEach(async (element: string) => {
+      await fetch(
+        `https://jsonmock.hackerrank.com/api/movies/search/?imdbID=${element}`
+      )
+        .then((res) => res.json())
+        .then((json: Info) => {
+          favouriteList.value.push(json.data[0]);
+        })
+        .catch((err) => (error.value = err));
+    });
+
+    return favouriteList.value;
+  }
+
+  return {
+    favouriteData,
+    setFavourite,
+    removeFavourite,
+    getDataFavourite,
+  };
 }
