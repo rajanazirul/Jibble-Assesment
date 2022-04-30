1c59c601640e52e0336fe4528a8261d1392ec7ed
rajanazirul <rajanazirul93@gmail.com>
2022-05-01 04:36:12 +0800

    revert vuetify setting

--- movie-listing/src/views/HomeView.vue
+++ movie-listing/src/views/HomeView.vue
@@ -1,12 +1,9 @@
 <template>
-  <search-bar />
   <hello-world />
-  <table-renderer :filter="{title: `as`,
-  pages: 0}"/>
 </template>
 
 <script setup lang="ts">
-import SearchBar from "../components/SearchBar.vue";
+// import SearchBar from "../components/SearchBar.vue";
 import HelloWorld from "../components/HelloWorld.vue";
-import TableRenderer from "../components/TableRenderer.vue";
+// import TableRenderer from "../components/TableRenderer.vue";
 </script>
