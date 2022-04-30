1640c2b8d7f3ca3e7a59a01668f197e146661980
rajanazirul <rajanazirul93@gmail.com>
2022-04-30 18:17:01 +0800

    revert back vite-config

---
+++ movie-listing/src/components/__tests__/HelloWorld.spec.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from "vitest";
+import HelloWorld from "../HelloWorld.vue";
+import { mount } from "@vue/test-utils";
+
+describe("SearchBar", () => {
+ 
+
+  it("renders properly", () => {
+    const wrapper = mount(HelloWorld, {
+      props: { msg: "Hello Vitest" },
+    });
+    expect(wrapper.text()).toContain(",");
+  });
+});
--- movie-listing/vite.config.ts
+++ movie-listing/vite.config.ts
@@ -1,35 +1,15 @@
+import { fileURLToPath, URL } from "url";
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vuetify from "@vuetify/vite-plugin";
-
-const path = require("path");
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-    }),
-  ],
-  define: { "process.env": {} },
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
-  resolve: {
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ]
-  },
-  */
 });
