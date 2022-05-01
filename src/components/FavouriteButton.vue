9823e3641124365aa91cb1eab093f988bad7fbc0
rajanazirul <rajanazirul93@gmail.com>
2022-05-01 19:27:10 +0800

    add favourite button component

---
+++ src/components/FavouriteButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <button v-if="item.status" type="button" @click="emits(`remove`)">
+    <i class="mdi mdi-star"></i>
+  </button>
+  <button v-else class="red" type="button" @click="emits(`add`)">
+    <i class="mdi mdi-star"></i>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from "@vue/runtime-core";
+
+const props = defineProps({
+  item: {
+    status: {
+      type: Boolean,
+    },
+    imdbId: {
+      type: String,
+    },
+  },
+  filter: {
+    title: {
+      type: String,
+    },
+    pages: {
+      type: Number,
+    },
+  },
+});
+
+const emits = defineEmits(["add", "remove"]);
+
+const item = computed(() => {
+  return props.item;
+});
+</script>
+
+<style scoped>
+.red {
+  color: rgb(109, 2, 2);
+}
+</style>
