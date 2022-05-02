c3ba895e7cc182d4bcbd7c1d0aaeb23810a7cd5d
rajanazirul <rajanazirul93@gmail.com>
2022-05-03 06:37:02 +0800

    Update SearchBar.vue

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -5,10 +5,9 @@
         type="text"
         class="search-input"
         placeholder="Search.."
-        v-model="filter.title"
+        v-model="title"
       />
-
-      <button class="search-button" @click="emits(`submit`, filter)">
+      <button class="search-button" @click="emits(`submit`, title)">
         <i class="mdi mdi-magnify search-icon"></i>
       </button>
     </div>
@@ -16,13 +15,45 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from "@vue/runtime-core";
-import { Filter } from "../services/movie";
+import { ref } from "@vue/runtime-core";
 
-const filter = reactive<Filter>({
-  title: "",
-  pages: 1,
-});
+const title = ref();
 
 const emits = defineEmits(["submit"]);
 </script>
+
+<style scoped>
+.search-box {
+  width: 100%;
+  position: relative;
+  display: flex;
+}
+.search-input {
+  width: 100%;
+  padding: 10px;
+  border: 4px solid #111d5e;
+  border-radius: 10px 0 0 10px;
+  border-right: none;
+  outline: none;
+  font-size: 20px;
+  color: tomato;
+  background: none;
+}
+.search-button {
+  text-align: center;
+  height: 61px;
+  width: 40px;
+  outline: none;
+  cursor: pointer;
+  border: 4px solid #111d5e;
+  border-radius: 0 10px 10px 0;
+  border-left: none;
+  background: none;
+  font-size: 20px;
+  border-left: 4px solid #111d5e;
+}
+.container {
+  width: 100%;
+  position: relative;
+}
+</style>
